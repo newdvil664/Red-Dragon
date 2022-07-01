54e1be6f423fda88b4bdab12eef27ef94cbeadf8
newdvil664 <108429201+newdvil664@users.noreply.github.com>
2022-07-02 00:39:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,12 +23,12 @@ global.APIKeys = {apkey
 global.alivemsg = `My name is devil
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+923080613785'] //Owner number in aive msg
-global.premium = ['+923080613785'] //Owner Number info
-global.ownernomer = '+923080613785' //Owner Number <<<
+global.owner = ['923080613785'] //Owner number in aive msg
+global.premium = ['923080613785'] //Owner Number info
+global.ownernomer = '923080613785' //Owner Number <<<
 
-global.ownername = '😎𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖 //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎devilofking //Owner Name
+global.botname = ' ☬Devil༒king' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
